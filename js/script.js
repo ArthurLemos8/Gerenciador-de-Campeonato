00c90b3553fde6fbcb12282b2bd94c7b424c3795
Arthur <arthurlemos809@gmail.com>
2026-06-06 22:46:35 -0300

    Descrição das alterações

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 let times = [];
+let confrontos = [];
 
 const inputNome = document.getElementById("nomeTime");
 const btnCadastrar = document.getElementById("btnCadastrar");
 const listaTimes = document.getElementById("listaTimes");
 const totalTimes = document.getElementById("total-times");
+const btnSortear = document.getElementById("btnSortear");
+const listaConfrontos = document.getElementById("listaConfrontos");
 
 btnCadastrar.addEventListener("click", cadastrarTimes);
+btnSortear.addEventListener("click", gerarCampeonato);
 
 function cadastrarTimes() {
     const nome = inputNome.value.trim();
@@ -69,4 +73,55 @@ function editarTime(id) {
     time.nome = novoNome.trim();
 
     renderizarTimes();
+}
+
+function sortearConfrontos(){
+    confrontos = [];
+
+    const timesEmbaralhados = [...times];
+    timesEmbaralhados.sort(() =>Math.random() - 0.5);
+
+    for(let i=0; i< timesEmbaralhados.length; i+=2){
+        if(timesEmbaralhados[i+1]){
+            confrontos.push({
+                mandante: timesEmbaralhados[i].nome,
+                visitante: timesEmbaralhados[i+1].nome
+            });
+        }
+    }
+    renderizarConfrontos();
+}
+function renderizarConfrontos(){
+    listaConfrontos.innerHTML ="";
+    confrontos.forEach((confronto, index)=>{
+
+        const tr = document.createElement("tr");
+
+        tr.innerHTML = `
+        <td>${index+1}</td>
+        <td>${confronto.mandante}</td>
+        <td>${confronto.visitante}</td>
+        `;
+        listaConfrontos.appendChild(tr)
+    });
+    document.getElementById("total-jogos").textContent = confrontos.length;
+}
+function gerarCampeonato() {
+    confrontos = [];
+
+    for(let i = 0; i < times.length; i++) {
+
+        for(let j = i + 1; j < times.length; j++) {
+
+            confrontos.push({
+                mandante: times[i].nome,
+                visitante: times[j].nome,
+                golsMandante: null,
+                golsVisitante: null
+            });
+
+        }
+    }
+
+    renderizarConfrontos();
 }
